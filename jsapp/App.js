619c02f8682e80c946adb7ca043bbe16f8dd0b36
louishaymoz <louishaymoz@gmail.com>
2026-04-01 19:43:34 +0000

    cards added

--- jsapp/App.js
+++ jsapp/App.js
@@ -1,15 +1,40 @@
 import React, { useState } from 'react';
-import { View, Text, Button, TextInput, FlatList } from 'react-native';
+import { View, Text, Button, TextInput, FlatList, Image} from 'react-native';
+import { ScrollView } from 'react-native-web';
 
 
 export default function App() {
-
-  const onPressButton = () => {
-    console.log('Button pressed!');
-  };
+  const [blocks, setBlocks] = useState([
+    { id: 1, text: "Block1", imageUrl: "https://picsum.photos/200"},
+    { id: 2, text: "Block2", imageUrl: "https://picsum.photos/201"},
+    { id: 3, text: "Block3", imageUrl: "https://picsum.photos/200"}
+  ])
+  const [boxText, setBoxText] = useState('TEXT')
   const [text, setText] = useState('');
+  const [list, setList] = useState([
+    {key: 'January'},
+    {key: 'February'},
+    {key: 'March'},
+    {key: 'April'},
+    {key: 'May'},
+    {key: 'June'},
+    {key: 'July'},
+    {key: 'August'},
+    {key: 'September'},
+    {key: 'October'},
+    {key: 'November'},
+    {key: 'December'},
+  ]);
+  const onPressButton = (textFromCard) => {
+    if (list.some(item=> item.key === textFromCard)){
+      setList(list.filter(item=> item.key !==textFromCard));
+    } else {
+      setList([...list, {key:textFromCard}])
+    }    
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: 'center' }}>
+    <ScrollView>
       <View style={{ alignItems: 'center', marginTop: 50 }}>
         <Text style={{fontSize: 24, marginBottom: 0}}>SAMPL!</Text>
         <Button
@@ -25,26 +50,25 @@ export default function App() {
         />
         <Text style={{padding: 10, fontSize: 30}}>{text}</Text>
       </View>
-      <View style={{flex: 1,width: '100%', marginTop: 20 }}>
+      <View style={{height: 200, marginTop: 20, alignItems: 'center'}}>
         <FlatList
-        data={[
-          {key: 'January'},
-          {key: 'February'},
-          {key: 'March'},
-          {key: 'April'},
-          {key: 'May'},
-          {key: 'June'},
-          {key: 'July'},
-          {key: 'August'},
-          {key: 'September'},
-          {key: 'October'},
-          {key: 'November'},
-          {key: 'December'},
-        ]}
+        data={list}
         renderItem={({item}) => <Text style={{ padding: 2, fontSize: 30 }}>{item.key}</Text>}
       />
 
       </View>
-    </View>
+      <View style={{flex: 1, maxWidth: 450, flexDirection: 'row' , flexWrap: 'wrap', alignSelf: 'center'}}>
+        {blocks.map(item => {
+          return(
+            <View key={item.id} style={{width: 200, padding: 20, backgroundColor: '#ffc2c2', borderRadius: 10, alignItems: 'center', margin: 10}}>
+              <Image source={{uri: item.imageUrl}} style={{width: 150, height: 150, borderRadius: 10,marginBottom: 15}}/>
+              <Text style={{fontSize: 18, marginBottom: 15}}>{item.text}</Text>
+              <Button onPress={() => onPressButton(item.text)} title='+/-' color='#ff0000' />
+            </View>
+          );
+        })}
+        
+      </View>
+    </ScrollView>
   );
 } 
