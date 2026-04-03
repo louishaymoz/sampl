acf5a4ecf1971925b63661137301b36232d3f62e
louishaymoz <louishaymoz@gmail.com>
2026-04-03 21:31:31 +0000

    added icon, also image to cardscreen'

--- jsapp/screens/CardScreen.js
+++ jsapp/screens/CardScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 export default function CardScreen({ route }) {
   const { card } = route.params;
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: 'center' }}>
+      <Image source={{uri: card.imageUrl}} style={{width: 300, height: 300, borderRadius: 10,marginBottom: 15, marginTop: 15}}/>
       <Text style={{ fontSize: 30 }}>{card.text}</Text>
     </View>
   );
